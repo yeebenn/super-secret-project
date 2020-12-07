29185e97ee8629930c340eac2bb13757e2e16314
Bennette Yee <bennetteyee@Bennettes-MacBook-Pro.local>
2020-12-08 11:33:42 +1300

    PR changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    # puts params.inspect
     @user = User.create(params.require(:user).permit(:username, :password))
       session[:user_id] = @user.id
-      redirect_to '/page_requires_login'
+      redirect_to '/authorized'
   end
 end
