49484babda9953a4f1b6b754cdb55feb9ee64c1c
Bennette Yee <bennetteyee@Bennettes-MacBook-Pro.local>
2020-12-08 13:50:33 +1300

    Added initial bootstrap, want to wo rk on making surveys and other things, will style after base of application is completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,32 @@
 class SessionsController < ApplicationController
-  skip_before_action :authorized, only: [:new, :create, :welcome]
-
-  def new
-  end
-
-  def create
-    @user = User.find_by(username: params[:username])
-
-   if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to '/welcome'
-   else
-      redirect_to '/login'
+   skip_before_action :authorized, only: [:new, :create, :welcome]
+ 
+   def new
    end
-
-  end
-
-  def destroy
-      session[:user_id] = nil
-      redirect_to '/login'
-  end
-
-  def page_requires_login
-  end
-
-  def login
-  end
-
-  def welcome
-  end
-end
+ 
+   def create
+     @user = User.find_by(username: params[:username])
+ 
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to '/welcome'
+    else
+       redirect_to '/login'
+    end
+ 
+   end
+ 
+   def destroy
+       session[:user_id] = nil
+       redirect_to '/login'
+   end
+ 
+   def page_requires_login
+   end
+ 
+   def login
+   end
+ 
+   def welcome
+   end
+ end
